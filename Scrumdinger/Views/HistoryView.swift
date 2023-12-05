63eb2f3b966ef049afe008fc34c6951d1baf2bae
Matt <maweiche@gmail.com>
2023-12-05 13:55:24 +0100

    added finished build of scrumdinger mobile app

---
+++ Scrumdinger/Views/HistoryView.swift
@@ -0,0 +1,52 @@
+//
+//  HistoryView.swift
+//  Scrumdinger
+//
+//  Created by Matt Weichel on 11/21/23.
+//
+
+import SwiftUI
+
+struct HistoryView: View {
+    let history: History
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading) {
+                Divider()
+                    .padding(.bottom)
+                Text("Attendees")
+                    .font(.headline)
+                Text(history.attendeeString)
+                if let transcript = history.transcript {
+                    Text("transcript")
+                        .font(.headline)
+                        .padding(.top)
+                    Text(transcript)
+                }
+            }
+        }
+        .navigationTitle(Text(history.date, style: .date))
+        .padding()
+    }
+}
+
+extension History {
+    var attendeeString: String {
+        ListFormatter.localizedString(byJoining: attendees.map { $0.name })
+    }
+}
+
+struct HistoryView_Preview: PreviewProvider {
+    static var history: History {
+        History(attendees: [
+            DailyScrum.Attendee(name: "Jon"),
+            DailyScrum.Attendee(name: "Darla"),
+            DailyScrum.Attendee(name: "Luis")
+        ],
+                transcript: "Darla, would you like to start today?")
+    }
+    static var previews: some View {
+        HistoryView(history: history)
+    }
+}
